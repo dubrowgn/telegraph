553b241d0f0fbc36c34f5f441fced420981adae5
Dustin Brown <dustin.brown@ul.com>
2015-05-07 17:03:26 -0600

    Initial rewrite after fork

--- README.md
+++ README.md
@@ -1,2 +1,103 @@
-# telegraph
-A minimalistic javascript event emitter based on smokesignals.js
+Telegraph
+=============
+
+A minimal event emitter for browsers, forked from Benjamin Thomas' [smokesignals.js][1].
+
+This library has three goals:
+
+1. Make it easy and intuitive to listen for and initiate events on an object.
+2. Be as small as possible. Right now the minified version comes in at 514 bytes.
+3. Capability for handlers to cancel the event process.
+
+There are many other [wonderful libraries that do similar things][2], but none
+of them worked exactly how I wanted them to work or met all the goals above.
+
+Installing
+----------
+
+Just download `telegraph.js` and put it in a place you can access from your webpage.
+
+Loading
+-------
+
+Just include the Telegraph script:
+
+    <script src="telegraph.js"></script>
+
+Using
+-----
+
+Make any object an event emitter:
+
+    var jill = {};
+    telegraph(jill);
+
+    // or...
+    var jill = telegraph();
+
+Or if you prefer constructors:
+
+    function Person() {
+        telegraph(this);
+    }
+    var jill = new Person();
+
+Now you can listen for events:
+
+    function listener(name) {
+        window.alert('Hello ' + name + '!');
+    }
+    jill.on('say hello', listener);
+
+And emit events:
+
+    jill.emit('say hello', 'Jack');
+    // alerts: "Hello Jack!"
+    // returns: true
+
+And remove a listener:
+
+    jill.off('say hello', listener);
+
+Or if you only want to listen for an event once:
+
+    jill.once('another event', function() {
+        window.alert("I'll only be called once!");
+    });
+    jill.emit('another event');
+
+Or remove all listeners for an event:
+
+    jill.off('say hello');
+
+Or if you want to remove ALL listeners:
+
+    // just call off() with no parameters
+    jill.off();
+
+    // or reconvert the object...
+    telegraph(jill);
+
+Or if you want to cancel the event chain:
+
+    // just return false from a handler
+    jill.on('event', function() { return false; });
+    jill.on('event', function() { console.log('event!'); });
+
+    // emit now returns false, and 'event!' is not printed
+    jill.emit('event');
+
+That's it! One global object (`telegraph`) and when used it adds 4 methods to
+your objects (`on`, `once`, `off` and `emit`).
+
+By the way, all methods, except for `emit`, are chainable:
+
+    var jill = smokesignals.convert({})
+      .on('event one', function() { ... })
+      .on('event two', function() { ... })
+      .once('event three', function() { ... })
+      .off ('event one')
+      ;
+
+[1]: https://bitbucket.org/bentomas/smokesignals.js
+[2]: http://microjs.com/#events
---
+++ telegraph.js
@@ -0,0 +1,114 @@
+/**
+ * Converts the passed object, or a new object, into an event emitter
+ * @param {object} obj The object to convert
+ * @returns {object} The passed object for chaining
+ */
+telegraph = function (obj) {
+    'use strict';
+
+    obj = obj || {};
+
+    // we store the list of handlers as a local variable inside the scope so
+    // that we don't have to add random properties to the object we are
+    // wrapping. (prefixing variables in the object with an underscore or two is
+    // an ugly solution)
+    var handlers = {};
+
+    /**
+     * Add a listener
+     * @param {string} eventName The name of the event
+     * @param {function} handler The handler function for the event
+     * @returns {object} This object for chaining
+     */
+    obj.on = function (eventName, handler, front) {
+        // either use the existing array or create a new one for this event
+        (handlers[eventName] = handlers[eventName] || [])
+            // add the handler to the array
+            [front ? 'unshift' : 'push'](handler);
+
+        return obj;
+    };
+
+    /**
+     * Add a listener that will only be called once
+     * @param {string} eventName The name of the event
+     * @param {function} handler The handler function for the event
+     * @returns {object} This object for chaining
+     */
+    obj.once = function (eventName, handler, front) {
+        // create a wrapper listener, that will remove itself after it is called
+        function wrappedHandler() {
+            // remove ourself, and then call the real handler with the args
+            // passed to this wrapper
+            handler.apply(obj.off(eventName, wrappedHandler), arguments);
+        }
+
+        // in order to allow that these wrapped handlers can be removed by
+        // removing the original function, we save a reference to the original
+        // function
+        wrappedHandler.h = handler;
+
+        // call the regular add listener function with our new wrapper
+        return obj.on(eventName, wrappedHandler, front);
+    };
+
+    /**
+     * Remove a listener. Remove all listeners for eventName if handler is
+     * omitted. Remove all listeners for all event names if eventName is also
+     * omitted.
+     * @param {string} eventName The name of the event
+     * @param {function} handler The handler function for the event
+     * @returns {object} This object for chaining
+     */
+    obj.off = function (eventName, handler) {
+    	// if no eventName, clear all event handlers for all events
+    	if (eventName === undefined) {
+    		handlers = {};
+    		return obj;
+    	} // if
+
+        // loop through all handlers for this eventName to see if the handler
+        // passed in was any of them so we can remove it
+        //		if no handler, clear all handlers for the event instead
+        var list = handler ? handlers[eventName] || [] : [];
+        for (var i = 0; i < list.length; i++) {
+            // either this item is the handler passed in, or this item is a
+            // wrapper for the handler passed in.  See the 'once' function
+            if (list[i] === handler || list[i].h === handler)
+                list.splice(i--, 1);
+        } // for( i )
+
+        // cleanup if no events for the eventName
+        if (!list.length) {
+            // remove the array for this eventname (if it doesn't exist then
+            // this isn't really hurting anything)
+            delete handlers[eventName];
+        } // if
+
+        return obj;
+    };
+
+    /**
+     * Dispatches the named event, calling all registered handler functions. If
+     * any handler returns false, the event subsequent handlers are not called
+     * and false is returned; Otherwise, all handlers are called and true is
+     * returned.
+     * @param {string} eventName The name of the event to dispatch
+     * @returns {boolean} False if any handler returns false, true otherwise.
+     */
+    obj.emit = function (eventName) {
+        // loop through all handlers for this event name and call them all
+        //		arguments is "array-like", so call slice() from list instead
+        //		handlers can return false to cancel event
+        var list = handlers[eventName] || [];
+        var args = list.slice.call(arguments, 1);
+        for (var i = 0; i < list.length; ++i) {
+            if (list[i].apply(obj, args) === false)
+                return false;
+        } // for( i )
+
+        return true;
+    };
+
+    return obj;
+} // telegraph( )
